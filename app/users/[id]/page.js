f10c424490d940e78c492ba02dde7d152046eb82
Heidi-Astrup <eaa24hsas@students.eaaa.dk>
2025-11-13 14:52:38 +0100

    Refactor UserDetailPage to include post fetching and rendering functionality

--- app/users/[id]/page.js
+++ app/users/[id]/page.js
@@ -3,12 +3,23 @@ import Link from "next/link";
 import { redirect } from "next/navigation";
 import styles from "./page.module.css";
 import UserCard from "@/components/UserCard";
+import PostCard from "@/components/PostCard";
 
-export default async function UserPage({ params }) {
+export default async function UserDetailPage({ params }) {
   const { id } = await params;
   const url = `${process.env.NEXT_PUBLIC_FB_DB_URL}/users/${id}.json`;
   const response = await fetch(url);
   const user = await response.json();
+
+  const postUrl = `${process.env.NEXT_PUBLIC_FB_DB_URL}/posts.json?orderBy="uid"&equalTo="${id}"`;
+  const postResponse = await fetch(postUrl);
+  const dataObject = await postResponse.json();
+
+  const posts = Object.keys(dataObject).map((key) => ({
+    id: key,
+    ...dataObject[key],
+  })); // Convert object to array
+  console.log(posts);
 
   // Server Action to handle post deletion
   async function deletePost() {
@@ -34,6 +45,11 @@ export default async function UserPage({ params }) {
             <button className={styles.btnUpdate}>Update post</button>
           </Link>
         </div>
+        {posts.map((post) => (
+          <Link href={`/posts/${post.id}`} key={post.id}>
+            <PostCard post={post} />
+          </Link>
+        ))}
       </div>
     </main>
   );
